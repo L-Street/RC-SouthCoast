0f406d5e6f7821a4f8c0fe79ded78ecc10d50020
Lewis Street <lstreet@salisbury6c.ac.uk>
2021-03-23 12:19:07 +0000

    swap tabs

---
+++ RC.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", function () {
+    var changers = document.querySelectorAll(".swapper")
+    for (var i = 0; i < changers.length; i++) {
+        changers[i].addEventListener("click", function tab(event) {
+            var tabs = document.querySelectorAll(".fakepage")
+            var swaps = document.querySelectorAll(".swapper")
+            for (var i = 0; i < tabs.length; i++) {
+                tabs[i].style.display = "none"
+                swaps[i].src = "files/" + swaps[i].name + "grey.png"
+            }
+            event.target.src = "files/" + event.target.name + "blue.png"
+            document.querySelector("#" + event.target.name).style.display = "block"
+        })
+    }
+})
