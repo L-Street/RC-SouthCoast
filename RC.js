bed18662f04580359f6ff7f7654114e4b9ed6825
Lewis Street <lstreet@salisbury6c.ac.uk>
2021-03-25 11:59:42 +0000

    do point

--- RC.js
+++ RC.js
@@ -1,8 +1,51 @@
+var rules = {
+    "SORRCA Lite rules": {
+        "Progress": "0",
+        "Reverse": "0",
+        "Gate-Marker": "10",
+        "Rollover": "5",
+        "Boundary-Marker": "5",
+        "Vehicle-Touch": "5",
+        "Course-Direction": "0",
+        "Self-Recovery": "1",
+        "Assisted-Recovery": "5",
+        "Point-out": "",
+        "Did-not-finish": "point out + 10",
+        "Did-not-start": "point out + 20"
+    },
+    "Official Scale Rock Crawling Rules": {
+        "Progress": "-2",
+        "Reverse": "1",
+        "Gate-Marker": "10",
+        "Rollover": "5",
+        "Boundary-Marker": "10",
+        "Vehicle-Touch": "10",
+        "Course-Direction": "10",
+        "Self-Recovery": "3",
+        "Assisted-Recovery": "10",
+        "Point-out": "80",
+        "Did-not-finish": "100",
+        "Did-not-start": "130"
+    },
+    "Custom": {
+        "Progress": "",
+        "Reverse": "",
+        "Gate-Marker": "",
+        "Rollover": "",
+        "Boundary-Marker": "",
+        "Vehicle-Touch": "",
+        "Course-Direction": "",
+        "Self-Recovery": "",
+        "Assisted-Recovery": "",
+        "Point-out": "",
+        "Did-not-finish": "",
+        "Did-not-start": ""
+    }
+}
 var baselin = ["Gates", "Progress"]
-var penalin = ["Reverse", "Gate Marker", "Rollover", "Boundary Marker", "Vehicle Touch", "Course Direction", "Self Recovery", "Assisted Recovery", "Point out", "Did not finish", "Did not start"]
+var penalin = ["Reverse", "Gate-Marker", "Rollover", "Boundary-Marker", "Vehicle-Touch", "Course-Direction", "Self-Recovery", "Assisted-Recovery", "Point-out", "Did-not-finish", "Did-not-start"]
 window.addEventListener("DOMContentLoaded", function () {
-    var het = document.querySelector("#pulldon").style.height
-    console.log(het)
+
     var changers = document.querySelectorAll(".swapper")
     for (var i = 0; i < changers.length; i++) {
         changers[i].addEventListener("click", function tab(event) {
@@ -19,6 +62,14 @@ window.addEventListener("DOMContentLoaded", function () {
     var rulic = document.querySelectorAll(".ruleset")
     for (var i = 0; i < rulic.length; i++) {
         rulic[i].addEventListener("click", function rul(event) {
+            console.log(rules[event.target.innerText])
+            document.querySelector("#Gates").value = ""
+            console.log(rules[event.target.innerText][Progress])
+            document.querySelector("#Progress").value = rules[event.target.innerText]["Progress"]
+            for (var i = 0; i < penalin.length; i++) {
+                document.querySelector("#" + penalin[i]).value = rules[event.target.innerText][penalin[i]]
+            }
+
         })
     }
     for (var i = 0; i < baselin.length; i++) {
@@ -40,7 +91,7 @@ window.addEventListener("DOMContentLoaded", function () {
         var pair = document.createElement("div")
         pair.classList.add("pair")
         var label = document.createElement("p")
-        label.innerText = penalin[i]
+        label.innerText = penalin[i].replace(/-/g, ' ')
         label.classList.add("label")
         var valu = document.createElement("input")
         valu.classList.add("valu")
