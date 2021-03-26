68e5f88056e9a719691d34ddce69ec316078e0e6
Lewis Street <lstreet@salisbury6c.ac.uk>
2021-03-26 14:15:02 +0000

    almost scale

--- RC.js
+++ RC.js
@@ -1,3 +1,5 @@
+var points = 0
+var scalemeasure = 0
 var rules = {
     "SORRCA Lite rules": {
         "Progress": "0",
@@ -44,6 +46,7 @@ var rules = {
 }
 var baselin = ["Gates", "Progress"]
 var penalin = ["Reverse", "Gate-Marker", "Rollover", "Boundary-Marker", "Vehicle-Touch", "Course-Direction", "Self-Recovery", "Assisted-Recovery", "Point-out", "Did-not-finish", "Did-not-start"]
+var valos = ["Progress", "Reverse", "Gate-Marker", "Rollover", "Boundary-Marker", "Vehicle-Touch", "Course-Direction", "Self-Recovery", "Assisted-Recovery"]
 window.addEventListener("DOMContentLoaded", function () {
 
     var changers = document.querySelectorAll(".swapper")
@@ -62,9 +65,7 @@ window.addEventListener("DOMContentLoaded", function () {
     var rulic = document.querySelectorAll(".ruleset")
     for (var i = 0; i < rulic.length; i++) {
         rulic[i].addEventListener("click", function rul(event) {
-            console.log(rules[event.target.innerText])
             document.querySelector("#Gates").value = ""
-            console.log(rules[event.target.innerText][Progress])
             document.querySelector("#Progress").value = rules[event.target.innerText]["Progress"]
             for (var i = 0; i < penalin.length; i++) {
                 document.querySelector("#" + penalin[i]).value = rules[event.target.innerText][penalin[i]]
@@ -101,5 +102,115 @@ window.addEventListener("DOMContentLoaded", function () {
         pair.appendChild(label)
         pair.appendChild(valu)
         document.querySelector("#penalin").appendChild(pair)
+    }
+    document.querySelector("#plus").addEventListener("click", function newd(event) {
+        var thedrop = document.querySelector(".modsele")
+        var clone = thedrop.cloneNode(true)
+        clone.classList.add("clone")
+        var plus = event.target
+        plus.remove()
+        document.querySelector("#modi").appendChild(clone)
+        document.querySelector("#modi").appendChild(plus)
+    })
+    document.querySelector("#resetscale").addEventListener("click", function noclone(event) {
+        var clones = document.querySelectorAll(".clone")
+        for (var i = 0; i < clones.length; i++) {
+            clones[i].remove()
+        }
+        if (document.querySelector("#scalestat").innerText === "OFF") { }
+        else {
+            var check = parseInt(document.querySelector("#model").options[document.querySelector("#model").selectedIndex].value)
+            var factors = document.querySelectorAll(".modsele")
+            scalemeasure = 0
+            points = 0
+            for (var i = 0; i < factors.length; i++) {
+                var attrib = parseInt(factors[i].options[factors[i].selectedIndex].value)
+                scalemeasure += attrib
+            }
+            if (scalemeasure < check) {
+                console.log("max")
+                points += check
+                scalemeasure = check
+                document.querySelector("#total").innerText = points
+            } else {
+                points += scalemeasure
+                console.log("safe")
+                document.querySelector("#total").innerText = points
+            }
+        }
+    })
+    document.querySelector("#scalestat").addEventListener("click", function statuz(event) {
+        if (document.querySelector("#scalestat").innerText === "OFF") {
+            document.querySelector("#scalestat").innerText = "ON"
+            document.querySelector("#scalestat").style.backgroundColor = "rgba(21, 236, 17,0.64)"
+            var check = parseInt(document.querySelector("#model").options[document.querySelector("#model").selectedIndex].value)
+            var factors = document.querySelectorAll(".modsele")
+            scalemeasure = 0
+            points = 0
+            for (var i = 0; i < factors.length; i++) {
+                var attrib = parseInt(factors[i].options[factors[i].selectedIndex].value)
+                scalemeasure += attrib
+            }
+            if (scalemeasure < check) {
+                console.log("max")
+                points += check
+                scalemeasure = check
+                document.querySelector("#total").innerText = points
+            } else {
+                points += scalemeasure
+                console.log("safe")
+                document.querySelector("#total").innerText = points
+            }
+        }
+        else {
+            document.querySelector("#scalestat").innerText = "OFF"
+            document.querySelector("#scalestat").style.backgroundColor = "rgba(246, 14, 14,0.63)"
+            points -= scalemeasure
+            document.querySelector("#total").innerText = points
+        }
+    })
+    var losten = document.querySelectorAll(".modsele")
+    console.log(losten)
+    for (var i = 0; i < losten.length; i++) {
+        losten[i].addEventListener("change", function scal(event) {
+            if (document.querySelector("#scalestat").innerText === "OFF") { }
+            else {
+                var check = parseInt(document.querySelector("#model").options[document.querySelector("#model").selectedIndex].value)
+                var factors = document.querySelectorAll(".modsele")
+                points = 0
+                scalemeasure = 0
+                for (var i = 0; i < factors.length; i++) {
+                    var attrib = parseInt(factors[i].options[factors[i].selectedIndex].value)
+                    scalemeasure += attrib
+                }
+                if (scalemeasure < check) {
+                    console.log("max")
+                    points += check
+                    scalemeasure = check
+                    document.querySelector("#total").innerText = points
+                } else {
+                    points += scalemeasure
+                    console.log("safe")
+                    document.querySelector("#total").innerText = points
+                }
+            }
+        })
+    }
+
+    var lens = document.querySelectorAll(".line")
+    for (var i = 0; i < lens.length; i++) {
+        lens[i].addEventListener("change", function arl(event) {
+            var score = points
+            for (var i = 0; i < valos.length; i++) {
+                /*console.log(document.querySelector("#" + valos[i]))
+                console.log(document.querySelector("#" + valos[i] + "t"))*/
+                var change = document.querySelector("#" + valos[i]).value * document.querySelector("#" + valos[i] + "t").value
+                console.log(change)
+                score += change
+                console.log(score)
+            }
+
+            document.querySelector("#total").innerText = score
+        })
     }
 })
